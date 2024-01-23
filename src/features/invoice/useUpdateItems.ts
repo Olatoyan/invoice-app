c45e89238be040a81ba77fb0494bf275c5c090d9
Olatoyan <golatoyan@gmail.com>
2024-01-23 05:45:36 +0100

    fixed a little bug

--- src/features/invoice/useUpdateItems.ts
+++ src/features/invoice/useUpdateItems.ts
@@ -1,7 +1,7 @@
 import { useMutation } from "@tanstack/react-query";
 import { updateItemsRow } from "../../utils/helpers";
 import { ItemProps } from "../../types/Types";
-import toast from "react-hot-toast";
+// import toast from "react-hot-toast";
 
 export function useUpdateItems() {
   const { mutate: updateItems } = useMutation({
