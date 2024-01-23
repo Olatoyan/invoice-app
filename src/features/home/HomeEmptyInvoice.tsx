3e25b6fd84d8b4de9010093b13aa3bb6343dd23e
Olatoyan <golatoyan@gmail.com>
2024-01-23 02:13:38 +0100

    implemented dark mode on empty invoice page

--- src/features/home/HomeEmptyInvoice.tsx
+++ src/features/home/HomeEmptyInvoice.tsx
@@ -1,11 +1,18 @@
+import { useDarkMode } from "../../context/DarkModeContext";
+
 function HomeEmptyInvoice() {
+  const { isDarkMode } = useDarkMode();
   return (
     <div className="flex flex-col items-center justify-center text-center">
       <img src="./illustration-empty.svg" alt="Empty Invoice" />
-      <h2 className="pb-9 pt-28 text-[2.4rem] font-bold tracking-[-0.075rem] text-[#0c0e16]">
+      <h2
+        className={`pb-9 pt-28 text-[2.4rem] font-bold tracking-[-0.075rem] ${isDarkMode ? "text-white" : "text-[#0c0e16]"}`}
+      >
         There is nothing here
       </h2>
-      <p className="max-w-[24rem] text-[1.5rem] font-medium leading-[1.5rem] tracking-[-0.01rem] text-[#888eb0]">
+      <p
+        className={`max-w-[24rem] text-[1.5rem] font-medium leading-[1.5rem] tracking-[-0.01rem] ${isDarkMode ? "text-[#dfe3fa]" : "text-[#888eb0]"}`}
+      >
         Create an invoice by clicking the <br />
         <span className="font-bold">New Invoice</span> button and get started
       </p>
